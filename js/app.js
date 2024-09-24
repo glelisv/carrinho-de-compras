6bfb8011982b0b0a6839de2dcb032bf05481a748
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-09-24 16:55:05 -0300

    recuperando os elementos ao adicionar

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function adicionar() {
+    let nomeProduto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+    let valor = parseInt(document.getElementById('valor-total').value);
+
+    /*if(nomeProduto == "Fone de ouvido - R$100") {
+        //document.getElementById('lista-nomeProdutos').innerHTML = `<span class="texto-azul">${quantidade}x</span> Fone de ouvido <span class="texto-azul">R$100</span>`;
+        //total = total + (100 * quantidade);
+        alert(valor);
+    } else if (nomeProduto == "Celular - R$1400") {
+        alert("Celular");
+    } else if (nomeProduto == "Oculus VR - R$5000") {
+        alert("Oculus VR");
+    }*/
+}
+
+function limpar() {
+
+}

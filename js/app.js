692fef6f10b883f680c670a1a1a6ad06a9c8291a
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-09-24 17:20:43 -0300

    calculando valor total do carrinho

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
+let totalGeral = 0;
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML = '';
+let campoTotal = document.getElementById('valor-total');
+campoTotal.textContent = 'R$ 0';
+
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
-
     let preco = quantidade * valorUnitario;
-    let carrinho = document.getElementById('lista-produtos');
+    
     carrinho.innerHTML = carrinho.innerHTML + `        <section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`
-
+    
+    totalGeral += preco;
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = '';
 }
 
 function limpar() {
